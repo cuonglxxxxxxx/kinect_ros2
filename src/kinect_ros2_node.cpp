e6c20ee782a074e7c59b9ab9039dcbfad74a25c4
cuonglxxxxxxx <xc200k4@gmai.com>
2026-05-14 18:26:23 +0700

    fix dupilcate kinect node

--- src/kinect_ros2_node.cpp
+++ src/kinect_ros2_node.cpp
@@ -43,7 +43,7 @@ int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
 
-  rclcpp::executors::SingleThreadedExecutor exec;
+  rclcpp::executors::MultiThreadedExecutor exec;
   rclcpp::NodeOptions options;
   options.use_intra_process_comms(true);
 
